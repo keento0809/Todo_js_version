09d447bab89c84d4afc2369130024b720a6037e2
KEENTO <handball0321@gmail.com>
2021-12-29 22:47:14 -0800

    Add function (add task asynchronous

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Get DOM
 const title = document.querySelector("#title");
 const todoList = document.querySelector("#todoList");
+const todoInput = document.querySelector("[type=text]");
+const todoForm = document.querySelector(".todoForm");
+const addButton = document.querySelector("#submit");
+
+// Build out function
+function addList(e) {
+  e.preventDefault();
+  const newTask = todoInput.value;
+  createNewLi(newTask);
+}
+
+function createNewLi(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+  todoList.append(li);
+}
+
+// Hook up the event
+todoForm.addEventListener("submit", addList);
