05a288933d30f0b0c4fe0dd17af677e4b716eda0
KEENTO <handball0321@gmail.com>
2021-12-30 12:05:25 -0800

    Add a function to store the tasks on localStorage

--- index.js
+++ index.js
@@ -2,15 +2,54 @@
 const title = document.querySelector("#title");
 const todoList = document.querySelector("#todoList");
 const todoInput = document.querySelector("[type=text]");
-const todoForm = document.querySelector(".todoForm");
-const addButton = document.querySelector("#submit");
+const addTodo = document.querySelector(".todoForm");
+
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// const checks = document.querySelectorAll("[data-index]");
+// console.log(checks);
 
 // Build out function
 function addList(e) {
   e.preventDefault();
-  const newTask = todoInput.value;
-  createNewLi(newTask);
-  todoInput.value = "";
+  const text = todoInput.value;
+  const newTask = {
+    text,
+    done: false,
+  };
+  tasks.push(newTask);
+  createList(tasks, todoList);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  this.reset;
+  console.log(tasks);
+  // createNewLi(text);
+  // todoInput.value = "";
+}
+
+function createList(lists = [], todoList) {
+  todoList.innerHTML = lists
+    .map((task, i) => {
+      return `
+        <li>
+        <label for="task${i}">${task.text}</label>
+        <span>
+          <input type="checkbox" data-index=${i} id="task${i}" ${
+        task.done ? "checked" : ""
+      }/>
+        </span>
+        </li>`;
+    })
+    .join("");
+}
+
+function taskDone(e) {
+  // skip this unless it's an input
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  tasks[index].done = !tasks[index].done;
+  alert("Are you sure you've done this task?");
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  createList(tasks, todoList);
 }
 
 function createNewLi(task) {
@@ -20,5 +59,7 @@ function createNewLi(task) {
 }
 
 // Hook up the event
-todoForm.addEventListener("submit", addList);
-// todoInput.addEventListener("change", addList);
+addTodo.addEventListener("submit", addList);
+todoList.addEventListener("click", taskDone);
+
+createList(tasks, todoList);
